6858cb9b4f0c3c1d5ca4452bd3fa67b7213c89bd
HAMED310 <119097902+HAMED310@users.noreply.github.com>
2022-11-26 11:09:13 +0300

    Create gitVersion.h

---
+++ tools/gitVersion.h
@@ -0,0 +1,8 @@
+#ifndef _GIT_VERSION_HEADER 
+#define _GIT_VERSION_HEADER 
+// Auto generated - start 
+constexpr const char* kGitVersionString = ""; 
+#define GIT_VERSION_STRING "" 
+#define GIT_LAST_COMMIT "" 
+// Auto generated - end 
+#endif 
